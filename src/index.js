f835daf03a77a30ef04e93a06823dbb76444ea11
Kai Curry <kai@cape.io>
2016-02-07 16:16:15 -0600

    public exports

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Public API
+export {
+  HISTORY_CREATE,
+  HISTORY_RESTORE,
+  create as createHistory,
+  restore as restoreHistory
+} from './actions'
+export createHistoryCache from './historyCache'
+export historyMiddleware from './middleware'
+export historyReducer, {
+  selectActiveKey,
+  selectHistoryState,
+  makeHydratable,
+  getInitState
+} from './reducer'
+export syncHistoryWithStore from './sync'
+export { newKey, locationSerialize } from './utils'
