a3318f5181d2457752157207ce899c6fe378685c
Kai Curry <kai@cape.io>
2016-02-08 14:16:04 -0600

    comments on Link component

--- src/Link.js
+++ src/Link.js
@@ -2,14 +2,19 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { create } from './actions'
 
+// Really simple Link component to help transition from react-router.
+
 function mapStateToProps() {
   return {}
 }
 
+// @TODO Enable link to optionally call HISTORY_RESTORE action.
 function mapDispatchToProps(dispatch, ownProps) {
   const { href, to, hash } = ownProps
+  // This is called on click.
   function handleClick(event) {
     event.preventDefault()
+    // Dispatch our event.
     return dispatch(create({
       pathname: href || to,
       hash,
@@ -20,6 +25,7 @@ function mapDispatchToProps(dispatch, ownProps) {
   }
 }
 
+// Simply attaches the dispatch wrapped handleClick() above.
 function Anchor({ onClick, ...props }) {
   return <a onClick={onClick} {...props} />
 }
